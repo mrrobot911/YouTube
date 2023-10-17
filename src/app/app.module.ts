a7f2da5ab789fc4e5069023c66966c2def732abb
mrrobot911 <kelwin2705@gmail.com>
2023-10-17 22:08:00 +0300

    feat: project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import HeaderComponent from './header/header.component';
+import SearchItemComponent from './search/search-item/search-item.component';
+import SearchResultsComponent from './search/search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchItemComponent,
+    SearchResultsComponent,
   ],
   imports: [
     BrowserModule,
