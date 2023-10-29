b6be53baa55cc2f4baca78a8e6f27fd100d37096
mrrobot911 <kelwin2705@gmail.com>
2023-10-29 14:23:27 +0300

    feat: filter html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import HeaderComponent from './header/header.component';
 import SearchItemComponent from './search/search-item/search-item.component';
 import SearchResultsComponent from './search/search-results/search-results.component';
+import SearchFieldComponent from './header/search-field/search-field.component';
+import AuthComponent from './header/auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +20,17 @@ import SearchResultsComponent from './search/search-results/search-results.compo
     HeaderComponent,
     SearchItemComponent,
     SearchResultsComponent,
+    SearchFieldComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/header/search-field/search-field.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'youtube-search-field',
+  templateUrl: './search-field.component.html',
+  styleUrls: ['./search-field.component.scss'],
+})
+export default class SearchFieldComponent {
+
+}
