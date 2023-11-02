aa320d8c7bfca56236e377041f86ee5bab822503
mrrobot911 <kelwin2705@gmail.com>
2023-11-02 19:03:14 +0300

    fix: destructig iterface

--- src/app/search/search-item.model.ts
+++ src/app/search/search-item.model.ts
@@ -1,40 +1,44 @@
-export interface SearchItem {
-  kind: string,
-  etag: string,
-  id: string,
-  snippet: {
-    publishedAt: string,
-    channelId: string,
-    title: string,
-    description: string,
-    thumbnails: {
-      default: Thumbnails,
-      medium: Thumbnails,
-      high: Thumbnails,
-      standard: Thumbnails,
-      maxres: Thumbnails
-    },
-    channelTitle: string,
-    tags: Array<string>,
-    categoryId: string,
-    liveBroadcastContent: string,
-    defaultLanguage?: string,
-    localized: {
-      title: string,
-      description: string
-    },
-    defaultAudioLanguage: string,
-  }
-  statistics: {
-    viewCount: string,
-    likeCount: string,
-    dislikeCount: string,
-    favoriteCount: string,
-    commentCount: string
-  }
-}
 type Thumbnails = {
   url: string,
   width: number,
   height: number,
 };
+interface ThumbnailsList {
+  default: Thumbnails,
+  medium: Thumbnails,
+  high: Thumbnails,
+  standard: Thumbnails,
+  maxres: Thumbnails
+}
+type Statistics = {
+  viewCount: string,
+  likeCount: string,
+  dislikeCount: string,
+  favoriteCount: string,
+  commentCount: string
+};
+type Localized = {
+  title: string,
+  description: string
+};
+interface Snippet {
+  publishedAt: string,
+  channelId: string,
+  title: string,
+  description: string,
+  thumbnails: ThumbnailsList,
+  channelTitle: string,
+  tags: Array<string>,
+  categoryId: string,
+  liveBroadcastContent: string,
+  defaultLanguage?: string,
+  localized: Localized,
+  defaultAudioLanguage: string,
+}
+export interface SearchItem {
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: Snippet,
+  statistics: Statistics,
+}
--- src/app/search/search-response.model.ts
+++ src/app/search/search-response.model.ts
@@ -1,11 +1,12 @@
 import { SearchItem } from './search-item.model';
 
+interface PageInfo {
+  totalResults: number,
+  resultsPerPage: number,
+}
 export interface SearchResponce {
   kind: string,
   etag: string,
-  pageInfo: {
-    totalResults: number,
-    resultsPerPage: number
-  },
+  pageInfo: PageInfo,
   items: Array<SearchItem>
 }
